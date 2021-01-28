7f885666f2ab1a5ebd49541b33d20c014d44e240
yaken99 <inat002asb@gmail.com>
2021-01-28 18:31:34 +0900

    関数化

--- distance.cs
+++ distance.cs
@@ -4,16 +4,22 @@ class distance
 {
     static void Main()
     {
-        double lat1,lng1,lat2,lng2,dist;
-        lat1 = double.Parse(Console.ReadLine());
-        lng1 = double.Parse(Console.ReadLine());
-        lat2 = double.Parse(Console.ReadLine());
-        lng2 = double.Parse(Console.ReadLine());
+        double ilat1,ilng1,ilat2,ilng2;
+        ilat1 = double.Parse(Console.ReadLine());
+        ilng1 = double.Parse(Console.ReadLine());
+        ilat2 = double.Parse(Console.ReadLine());
+        ilng2 = double.Parse(Console.ReadLine());
 
+        Console.WriteLine(dist_hubeny(ilat1,ilng1,ilat2,ilng2));
+    }
+
+    static double dist_hubeny(double lat1,double lng1,double lat2,double lng2)
+    {
         double WGS84_A = 6378137.000;
         double WGS84_E2 = 0.0066943799901975;
         double WGS84_MNUM = 6335439.327292462;
         double P,W,W3,M,N,dx,dy;
+        double dist;
 
         P = ((lat1+lat2)/2) * Math.PI / 180;
         W = Math.Sqrt(1-WGS84_E2*Math.Pow(Math.Sin(P),2));
@@ -24,8 +30,7 @@ class distance
         dy = (lat1-lat2) * Math.PI / 180;
 
         dist = Math.Sqrt(Math.Pow(dy*M,2) + Math.Pow(dx * N * Math.Cos(P),2)); 
-
-        Console.WriteLine(dist);
-    }
-
+        dist = Math.Round(dist,3);
+        return dist;
+    }    
 }
